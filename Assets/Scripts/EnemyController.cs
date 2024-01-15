893321d0acf241b5b2811502f1f0a1e285ebfb89
Krishiv Manyam <krishivmanyam@gmail.com>
2024-01-14 21:00:34 -0800

    Added Enemy movement

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -10,8 +10,9 @@ public class EnemyController : MonoBehaviour
     private int currentMadness;
     public int maxHealth;
     public float thrust;
+    public float moveSpeed;
     Rigidbody2D rb2d;
-    GameObject player;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
@@ -20,18 +21,30 @@ public class EnemyController : MonoBehaviour
         currHealth = maxHealth;
         rb2d = gameObject.GetComponent<Rigidbody2D>();
         player  = GameObject.FindGameObjectWithTag("Player");
+        moveSpeed = 2.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentMadness = madnessBar.getMadness();
+        //currentMadness = madnessBar.getMadness();
         if (currHealth <= 0) {
             player.GetComponent<PlayerMovement>().damageBuff++;
             player.GetComponent<PlayerMovement>().maxHealth += player.GetComponent<PlayerMovement>().damageBuff*3;
             player.GetComponent<PlayerMovement>().currHealth += player.GetComponent<PlayerMovement>().damageBuff*3;
-            madnessBar.SetMadness(currentMadness + 10);
+            //madnessBar.SetMadness(currentMadness + 10);
             Destroy(gameObject);
+        }
+        MoveTowardsPlayer();
+    }
+    void MoveTowardsPlayer()
+    {
+        if (player != null)
+        {
+            Vector3 direction = player.transform.position - transform.position;
+            direction.Normalize();
+
+            rb2d.velocity = new Vector2(direction.x * moveSpeed, rb2d.velocity.y);
         }
     }
 
